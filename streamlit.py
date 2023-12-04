8cba8d312a379351af461bd07c5ce2226902d33b
piovesanaedoardo <piovesana.edoardo@gmail.com>
2023-12-04 16:36:04 +0100

    added menu

--- streamlit.py
+++ streamlit.py
@@ -11,18 +11,25 @@ import seaborn as sns
 # load data
 df = pd.read_csv('winequality-red.csv')
 
+# sidebar
+st.sidebar.header('Menù')
+
 # title
 st.title('Wine Quality Prediction')
 
 # header
-st.header('Dataframe')
-st.write(df)
+if st.sidebar.checkbox('DataFrame'):
+    st.title('Dataframe')
+    st.write(df)
 
 # Description
-st.subheader('Dataframe Description')
-st.write(df.describe().T)
+if st.sidebar.checkbox('Description'):
+    st.subheader('Dataframe Description')
+    st.write(df.describe().T)
 
 # Correlation Matrix
-st.subheader('Correlation Matrix')
-sns.heatmap(df.corr(), annot=True, fmt='.1f')
-st.pyplot(plt)
+if st.sidebar.checkbox('Correlation Matrix'):
+    st.subheader('Correlation Matrix')
+    sns.heatmap(df.corr(), annot=True, fmt='.1f')
+    st.pyplot(plt)
+
