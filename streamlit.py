b803275edc42272bffc3a42e21164445c5894385
piovesanaedoardo <piovesana.edoardo@gmail.com>
2023-12-04 16:25:31 +0100

    added correlation matrix

--- streamlit.py
+++ streamlit.py
@@ -12,12 +12,17 @@ import seaborn as sns
 df = pd.read_csv('winequality-red.csv')
 
 # title
-# st.title('Wine Quality Prediction')
+st.title('Wine Quality Prediction')
 
 # header
-st.header('Data Information:')
-st.write(df.describe())
-
-# show data as table
-st.header('Data:')
+st.header('Dataframe')
 st.write(df)
+
+# Description
+st.subheader('Dataframe Description')
+st.write(df.describe().T)
+
+# Correlation Matrix
+st.subheader('Correlation Matrix')
+sns.heatmap(df.corr(), annot=True, fmt='.1f')
+st.pyplot(plt)
